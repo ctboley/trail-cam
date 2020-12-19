874bc1c8cde63ebe79548c8ff4e3df5ce38e45f3
Clay Boley <ctboley@gmail.com>
2020-12-18 22:09:42 -0600

    Add email handler

--- integration/src/handler.js
+++ integration/src/handler.js
@@ -1,4 +1,34 @@
+const AWS = require("aws-sdk");
+const { simpleParser } = require("mailparser");
+const s3client = new AWS.S3({ region: "us-east-1" });
+
 module.exports.handler = async (event, context) => {
-  console.log('hello world')
-  return 'successful invocation'
-}
+  console.log("Received event");
+  for (const record of event.Records) {
+    console.log({ record });
+    const bucket = record.s3.bucket.name;
+    const key = record.s3.object.key;
+    console.log({ bucket, key });
+
+    const email = s3client.getObject({
+      Bucket: bucket,
+      Key: key,
+    });
+
+    const stream = email.createReadStream();
+    const parsed = await simpleParser(stream);
+    const attachments = parsed.attachments;
+
+    const promises = attachments.map((attachment) => {
+      const params = {
+        Bucket: bucket,
+        Key: `images/${attachment.filename}`,
+        Body: attachment.content,
+      };
+      return s3client.putObject(params).promise();
+    });
+
+    await Promise.all(promises);
+  }
+  return "successful invocation";
+};
