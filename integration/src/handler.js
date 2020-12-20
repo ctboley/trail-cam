f4c85b7adfb7e97960d9899b12130a9b5479d005
ctboley <ctboley@gmail.com>
2020-12-20 11:30:07 -0600

    Add tests and prettier config

--- integration/src/handler.js
+++ integration/src/handler.js
@@ -2,13 +2,17 @@ const AWS = require("aws-sdk");
 const { simpleParser } = require("mailparser");
 const s3client = new AWS.S3({ region: "us-east-1" });
 
-module.exports.handler = async (event, context) => {
-  console.log("Received event");
+const handler = async (event, context) => {
   for (const record of event.Records) {
-    console.log({ record });
     const bucket = record.s3.bucket.name;
     const key = record.s3.object.key;
-    console.log({ bucket, key });
+
+    if (!bucket) {
+      throw new Error("Bucket is required");
+    }
+    if (!key) {
+      throw new Error("Key is required");
+    }
 
     const email = s3client.getObject({
       Bucket: bucket,
@@ -32,3 +36,5 @@ module.exports.handler = async (event, context) => {
   }
   return "successful invocation";
 };
+
+module.exports = handler;
